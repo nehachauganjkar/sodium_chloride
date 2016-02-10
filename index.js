ffdb8773199ca6cd6f8c1f7e86e1987f93d36bbe
Neha Chauganjkar <nchaug@sapient.com>
2016-02-10 16:04:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var crypto = require('crypto');
+
+  var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0,length); 
+};
+
+
+var sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512', salt); 
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+  function saltHashPassword(userpassword) {
+    var salt = genRandomString(16); 
+    var passwordData = sha512(userpassword, salt);
+    console.log('userPassword = '+userpassword);
+        console.log('passwordhash = '+passwordData.passwordHash);
+    console.log('\nSalt = '+passwordData.salt);
+}
+
+saltHashPassword('Neha');
+saltHashPassword('Neha');
