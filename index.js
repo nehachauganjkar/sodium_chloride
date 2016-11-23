9c037bbe3ee64b7f09a2a3225b1c4154554aac55
Neha <nchauganjkar@gmail.com>
2016-11-23 12:35:59 +0530

    patch commit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 var crypto = require('crypto');
 
   var genRandomString = function(length){
-    return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0,length); 
+    return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0,length);
 };
 
 
 var sha512 = function(password, salt){
-    var hash = crypto.createHmac('sha512', salt); 
+    var hash = crypto.createHmac('sha512', salt);
     hash.update(password);
     var value = hash.digest('hex');
     return {
@@ -17,12 +17,7 @@ var sha512 = function(password, salt){
 };
 
 module.exports = function saltHashPassword(userpassword) {
-    var salt = genRandomString(16); 
+    var salt = genRandomString(16);
     var passwordData = sha512(userpassword, salt);
-    console.log('userPassword = '+userpassword);
-        console.log('passwordhash = '+passwordData.passwordHash);
-    console.log('\nSalt = '+passwordData.salt);
+    return passwordData;
 };
-
-//saltHashPassword(process.argv[2]);
-//saltHashPassword(process.argv[2]);
